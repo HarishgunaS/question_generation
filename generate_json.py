2fe391cafbcd446bf49cb70d3ae97b0a4c8eb7cc
HarishgunaS <harishguna.satgunarajah@mail.utoronto.ca>
2021-01-16 15:44:32 -0500

    added generate_json.py

---
+++ generate_json.py
@@ -0,0 +1,17 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+from pipelines import pipeline
+import json
+import sys
+if __name__ == "__main__":
+    if (len(sys.argv) > 1):
+        d = dict()
+        with open("../data/"+str(sys.argv[1])) as file:
+            input_string = file.read()
+            nlp = pipeline("question-generation")
+            l = nlp(input_string)
+            for i in range(len(l)):
+                d[i] = l[i]
+        with open("../data/questions.json", "w") as fp:
+            json.dump(d, fp)
+            
